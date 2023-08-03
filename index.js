19904f7a0b224f717b5198e9c5ac429662aca66f
ismi29ch <ismi29ch@gmail.com>
2023-08-03 06:33:31 +0500

    Error Handling Custom delay Auto Pick name from Url Handle edge cases

--- index.js
+++ index.js
@@ -15,15 +15,30 @@ const download = async (url, name) => {
 	a.remove();
 };
 
-export default async function multiDownload(urls, {rename} = {}) {
-	if (!urls) {
-		throw new Error('`urls` required');
+const getName = ({ url }) => {
+	const match = url.match(/\/([^/]+)$/);
+	return match ? match[1] : "file";
+};
+
+export default async function multiDownload(urls = [], options = {}) {
+	const { delay = 1000, rename = getName } = options;
+	if (!urls?.length) {
+		throw new Error("`urls` required");
+	}
+	if (urls.every((url) => typeof url !== "string")) {
+		throw new Error("`urls` must be an array of strings");
+	}
+	if (typeof delay !== "number") {
+		throw new Error("`delay` must be a number");
+	}
+	if (typeof rename !== "function") {
+		throw new Error("`rename` must be a function");
 	}
 
 	for (const [index, url] of urls.entries()) {
-		const name = typeof rename === 'function' ? rename({url, index, urls}) : '';
+		const name = rename({ url, index, urls });
 
-		await delay(index * 1000); // eslint-disable-line no-await-in-loop
+		await delay(index * delay);
 		download(url, name);
 	}
 }
