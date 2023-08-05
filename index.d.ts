ee18568f71184edf53d23595f97736718bd0094a
ismi29ch <ismi29ch@gmail.com>
2023-08-05 21:02:36 +0500

    update the ts definition based on provided guidelines examples

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,64 @@
-declare module "multi-download" {
+/**
+ * Function to rename downloaded files.
+ */
+export interface RenameFunction {
 	/**
-	 * Download multiple files at once
-	 * @param urls The urls to download
-	 * @param options The options
-	 * @param options.rename The function to rename the file
-	 * @param options.delayTime The delay time between each download
+	 * This function is used to determine the filename for each downloaded file.
+	 * @param options - The options for renaming.
+	 * @returns The new filename for the downloaded file.
 	 */
-	export default async function multiDownload(
-		urls: string[],
-		options?: {
-			rename?: (options: {
-				url: string;
-				index: number;
-				urls: string[];
-			}) => string;
-			delayTime?: number;
-		}
-	): void;
+	(options: { url: string; index: number; urls: string[] }): string;
 }
+
+/**
+ * Options for the multiDownload function.
+ */
+export interface MultiDownloadOptions {
+	/**
+	 * The function to rename the downloaded files.
+	 * @default undefined (use original filenames)
+	 */
+	rename?: RenameFunction;
+
+	/**
+	 * The delay time between each file download in milliseconds.
+	 * @default 1000
+	 */
+	delayTime?: number;
+}
+
+/**
+ * Download multiple files at once.
+ * @param urls The URLs to download.
+ * @param options The options for downloading.
+ * @param options.rename The function to rename the downloaded files.
+ * @param options.delayTime The delay time between each file download.
+ * @example
+ * ```typescript
+ * import { multiDownload } from 'multi-download';
+ *
+ * async function downloadFiles() {
+ *   const urls = [
+ *     'https://example.com/file1.txt',
+ *     'https://example.com/file2.txt',
+ *     'https://example.com/file3.txt',
+ *   ];
+ *
+ *   try {
+ *     await multiDownload(urls, {
+ *       rename: ({ url, index }) => `file${index + 1}.txt`,
+ *       delayTime: 1000,
+ *     });
+ *     console.log('Files downloaded successfully');
+ *   } catch (error) {
+ *     console.error('Error downloading files:', error);
+ *   }
+ * }
+ *
+ * downloadFiles();
+ * ```
+ */
+export default function multiDownload(
+	urls: string[],
+	options?: MultiDownloadOptions
+): Promise<void>;
